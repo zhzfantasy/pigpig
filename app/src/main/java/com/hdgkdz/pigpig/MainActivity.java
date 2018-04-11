002c1aac12738ef754972286a2f5bfceba2b6836
zhzfantasy <26131108+zhzfantasy@users.noreply.github.com>
2018-04-11 15:47:04 +0800

    new

--- app/src/main/java/com/hdgkdz/pigpig/MainActivity.java
+++ app/src/main/java/com/hdgkdz/pigpig/MainActivity.java
@@ -2,12 +2,14 @@ package com.hdgkdz.pigpig;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//不显示标题栏
         setContentView(R.layout.activity_main);
     }
 }
